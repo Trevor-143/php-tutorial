9c6fde80fc92341b948ad4abb54f82f566d76a82
Trevor_143 <trevorarapu2@gmail.com>
2024-10-26 10:42:46 +0300

    php started

---
+++ resources/views/about.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>About page</title>
+</head>
+<body class="font-sans antialiased dark:bg-black dark:text-white/50" >
+    <h1>Heeelllo from about page</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia dignissimos libero sit, quisquam expedita temporibus! Accusamus sed libero natus repellendus, eius, ratione, aliquam sapiente quis quas sunt architecto nobis itaque.</p>
+</body>
+</html>
---
+++ resources/views/contact.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>Contact Page</title>
+</head>
+<body class="font-sans antialiased dark:bg-black dark:text-white/50" >
+    <h1>Contact page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nostrum eius eum vel quam modi magnam iusto sint qui veniam ipsum dolorum aspernatur odit quae deserunt dolore, cumque porro enim magni eos et atque doloribus reprehenderit eveniet. Officiis, sit expedita ad magnam voluptate, cupiditate vero natus pariatur iure, nam ducimus?</p>
+</body>
+</html>
